c8cf38b2fb4c57a6690eab10c034186eb2a47269
Endrika Nanami <endrika.nanami@gmail.com>
2025-01-11 10:09:00 +0700

    Add tailwind cdn in template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>{{ config('app.name') }}
         @hasSection('title')
             - @yield('title')
