d290512cf956d91addb9948acd94724aa02b5630
Endrika Nanami <endrika.nanami@gmail.com>
2025-01-11 09:58:14 +0700

    Add empty signup page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
+
 class AuthController extends Controller
 {
-    //
+    public function getSignupPage(): View
+    {
+        return view('auth.signup');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,8 @@
 <?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('signup', [AuthController::class, 'getSignupPage'])->name('get_signup_page');
+});
